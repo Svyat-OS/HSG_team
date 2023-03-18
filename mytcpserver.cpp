7dac49f7c25f6165b7cb43f7648d515b60bcc378
ilyasuper777 <super_nail_228@mail.ru>
2023-03-18 23:40:56 +0300

    2 version

--- mytcpserver.cpp
+++ mytcpserver.cpp
@@ -36,14 +36,73 @@ void MyTcpServer::slotNewConnection(){
 
 void MyTcpServer::slotServerRead(){
     QTcpSocket* curr_mTcpSocket = ((QTcpSocket*)sender()); // узнаём сокет отправителя
-    while(curr_mTcpSocket->bytesAvailable()>0)
-    {
-        QByteArray array =curr_mTcpSocket->readAll();
-        curr_mTcpSocket->write(array);
+    QDataStream in(curr_mTcpSocket); in.setVersion(QDataStream::Qt_6_2);
+    if (in.status()==QDataStream::Ok){
+        qDebug()<<"read...";
+        QString str;
+        in>>str;
+        qDebug()<<str;
+        SendToClient(str, curr_mTcpSocket);
     }
+
+
 }
 
 void MyTcpServer::slotClientDisconnected(){
+
     QTcpSocket* curr_mTcpSocket = ((QTcpSocket*)sender());
     curr_mTcpSocket->close();
+}
+void MyTcpServer::SendToClient(QString str, QTcpSocket* curr_mTcpSocket){
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly); out.setVersion(QDataStream::Qt_6_2);
+    out <<parsing(str);
+    curr_mTcpSocket->write(Data);
+}
+QString MyTcpServer::parsing(QString str){
+    std::string ans = str.toStdString();
+    if (str=="task1"){
+        ans=task1();}
+    else if (str=="task2"){
+        ans=task2();}
+    else if (str=="task3"){
+        ans=task3();}
+    else if (str=="task4"){
+        ans=task4();}
+    else if (str=="authorization"){
+        ans=autor();}
+    else if (str=="registration"){
+        ans=reg();}
+    else if (str=="statistics"){
+        ans=stat();}
+    return QString::fromStdString(ans);
+}
+
+std::string MyTcpServer::task1(){
+    std::string ans=" ";
+    return ans;
+}
+std::string MyTcpServer::task2(){
+    std::string ans=" ";
+    return ans;
+}
+std::string MyTcpServer::task3(){
+    std::string ans=" ";
+    return ans;
+}
+std::string MyTcpServer::task4(){
+    std::string ans=" ";
+    return ans;
+}
+std::string MyTcpServer::autor(){
+    std::string ans=" ";
+    return ans;
+}
+std::string MyTcpServer::reg(){
+    std::string ans=" ";
+    return ans;
+}
+std::string MyTcpServer::stat(){
+    std::string ans=" ";
+    return ans;
 }
--- mytcpserver.h
+++ mytcpserver.h
@@ -3,11 +3,11 @@
 #include <QObject>
 #include <QTcpServer>
 #include <QTcpSocket>
-
+#include <QDataStream>
 #include <QtNetwork>
 #include <QByteArray>
 #include <QDebug>
-
+#include <QString>
 #include <QMap>
 
 class MyTcpServer : public QObject
@@ -23,9 +23,19 @@ public slots:
     void slotServerRead();
     //void slotReadClient();
 private:
+    QByteArray Data;
     QTcpServer * mTcpServer;
     QMap <int, QTcpSocket*> mTcpSocket;
     int server_status;
+    void SendToClient(QString str, QTcpSocket* curr_mTcpSocket);
+    QString parsing(QString str);
+    std::string task1();
+    std::string task2();
+    std::string task3();
+    std::string task4();
+    std::string autor();
+    std::string reg();
+    std::string stat();
 };
 #endif // MYTCPSERVER_H
 
